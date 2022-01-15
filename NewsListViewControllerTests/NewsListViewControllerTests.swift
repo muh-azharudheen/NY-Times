a626806d6f91269c962bd90df6b64e0c490cd7b6
Azharudheen <muhdazharudheen@gmail.com>
2022-01-15 11:14:47 +0400

    NewsListViewController does not call load on initialization

--- NewsListViewControllerTests/NewsListViewControllerTests.swift
+++ NewsListViewControllerTests/NewsListViewControllerTests.swift
@@ -9,4 +9,21 @@ import XCTest
 @testable import NYTimes
 
 class NewsListViewControllerTests: XCTestCase {
+    
+    func test_init_doesNotFetchNews() {
+        let (_, loader) = makeSUT()
+        XCTAssertEqual(loader.loadCallCount, 0)
+    }
+    
+    private func makeSUT() ->  (sut: NewsListViewController, loader: NewsListLoaderSpy) {
+        let loader = NewsListLoaderSpy()
+        let sut = NewsListViewController(loader: loader)
+        return (sut, loader)
+    }
+}
+
+class NewsListLoaderSpy: NewsListLoader {
+    
+    var loadCallCount = 0
+    
 }
