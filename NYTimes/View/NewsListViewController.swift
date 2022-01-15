ebbcd7d981dcd0cfe6f9d325faec24a9047cb0a4
Azharudheen <muhdazharudheen@gmail.com>
2022-01-15 10:26:06 +0400

    add viewModel as dependencyInjection

--- NYTimes/View/NewsListViewController.swift
+++ NYTimes/View/NewsListViewController.swift
@@ -7,11 +7,25 @@
 
 import UIKit
 
-class NewsListViewController: UIViewController {
-
+class NewsListViewController: UITableViewController {
+    
+    private var viewModel: NewsListViewModel
+    
+    init(viewModel: NewsListViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .white
     }
+}
+
+class NewsListViewModel {
+    
 }
