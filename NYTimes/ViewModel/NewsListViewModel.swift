ca95b3f9b047756bb5c42cbe7bd40fe8fa406881
Azharudheen <muhdazharudheen@gmail.com>
2022-01-17 00:42:19 +0400

    Create NewsListViewModel & NewsListViewModelTests

---
+++ NYTimes/ViewModel/NewsListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  NewsListViewModel.swift
+//  NYTimes
+//
+//  Created by Muhammed Azharudheen on 27/10/1400 AP.
+//
+
+import Foundation
+
+class NewsListViewModel {
+    
+}
---
+++ NYTimesTests/NewsListViewModelTests.swift
@@ -0,0 +1,13 @@
+//
+//  NewsListViewModelTests.swift
+//  NYTimesTests
+//
+//  Created by Muhammed Azharudheen on 27/10/1400 AP.
+//
+
+import XCTest
+@testable import NYTimes
+
+class NewsListViewModelTests: XCTestCase {
+    
+}
