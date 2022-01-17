5677d41657df4d2c6e23fea82321b56473913b81
Azharudheen <muhdazharudheen@gmail.com>
2022-01-17 11:38:35 +0400

    create DetailViewController Tests

---
+++ NYTimesTests/DetailViewControllerTests.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewControllerTests.swift
+//  NYTimesTests
+//
+//  Created by Muhammed Azharudheen on 27/10/1400 AP.
+//
+
+import XCTest
+@testable import NYTimes
+
+class DetailViewControllerTests: XCTestCase {
+    
+}
